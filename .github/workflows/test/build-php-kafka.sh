f10219d1ba9fc31b5afc6ff7706db1f649d80d74
nick <nickjobszh@gmail.com>
2020-12-30 23:09:44 +0100

    fix typo

--- .github/workflows/test/build-php-kafka.sh
+++ .github/workflows/test/build-php-kafka.sh
@@ -11,7 +11,7 @@ fi
 
 cd php-kafka
 phpize
-CFLAGS="PHP_KAFKA_CLIENT_CFLAGS" ./configure
+CFLAGS="$PHP_KAFKA_CLIENT_CFLAGS" ./configure
 make
 
 echo "extension=$(pwd)/modules/kafka_client.so"|sudo tee /usr/local/etc/php/kafka_client.ini >/dev/null
