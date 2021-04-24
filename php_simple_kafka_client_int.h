4084bf137d94ad9649be8a62560581c0f237a68d
nick <nickjobszh@gmail.com>
2021-04-24 18:19:32 +0200

    cleanup legacy consumer

--- php_simple_kafka_client_int.h
+++ php_simple_kafka_client_int.h
@@ -75,9 +75,7 @@ typedef struct _kafka_object {
     rd_kafka_type_t         type;
     rd_kafka_t              *rk;
     kafka_conf_callbacks    cbs;
-    HashTable               consuming;
 	HashTable				topics;
-	HashTable				queues;
     zend_object             std;
 } kafka_object;
 
